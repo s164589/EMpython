8005d90232e919491c4886586619f48282680060
svendsen991 <emilabildgaardsvendsen@gmail.com>
2018-11-12 19:56:03 +0100

    Test4

--- test1.py
+++ test1.py
@@ -4,10 +4,13 @@ import json
 
 
 mu_zero = 4 * math.pi * pow(10, -7)
+epsilon_zero = 8.854 * pow(10, -12)
 
 with open("materials.json", "r") as material_file:
     data = json.load(material_file)
 
 print(mu_zero)
+print(epsilon_zero)
 print(data["material"]["Gold"])
+print(data["material"]["Bismuth"])
 
