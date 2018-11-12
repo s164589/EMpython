df99cdaf5d6e4e78d734e7a3e60dd197c1757009
svendsen991 <emilabildgaardsvendsen@gmail.com>
2018-11-12 18:58:23 +0100

    tes3

--- test1.py
+++ test1.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 import math
+import json
+
 
 mu_zero = 4 * math.pi * pow(10, -7)
+
+with open("materials.json", "r") as material_file:
+    data = json.load(material_file)
+
+print(mu_zero)
+print(data["material"]["Gold"])
 
